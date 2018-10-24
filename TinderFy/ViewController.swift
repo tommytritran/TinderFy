57cec69ad99f52aa5112dfa7e01c3c4c7837431f
Tommy Tran <tommytran@tommys-macbook-pro.local>
2018-10-23 18:42:29 -0700

    Initial Commit

---
+++ TinderFy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderFy
+//
+//  Created by Tommy Tran on 23/10/2018.
+//  Copyright © 2018 Tommy Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
